95ee41a94a6295e372bb8c4ce7ea93c63540aae5
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2024-06-03 22:36:58 +0500

    1.1

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -3,12 +3,37 @@ package com.example.moviesearch
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.ViewGroup
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val content = findViewById<ViewGroup>(R.id.content)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "МЕНЮ", Toast.LENGTH_SHORT).show()
+        }
+
+        button_fav.setOnClickListener {
+            Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
+        }
+
+        button_watch.setOnClickListener {
+            Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
+        }
+
+        button_compilations.setOnClickListener {
+            Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
+        }
+
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
