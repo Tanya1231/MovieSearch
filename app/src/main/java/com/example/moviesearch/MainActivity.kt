cf626d7c9aa659007d1a99195c85d8c71b8e262f
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2024-07-01 15:00:47 +0500

    add list films

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -71,4 +71,64 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    // База с фильмами
+    data class Film(
+        val title: String,
+        val poster: Int,
+        val description: String
+    )
+
+    val filmsDataBase = listOf(
+        Film(
+            title = "Начало",
+            poster = R.drawable.beginning,
+            description = "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."
+        ),
+        Film(
+            title = "Интерстеллар",
+            poster = R.drawable.interstellar,
+            description = "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."
+        ),
+        Film(
+            title = "Паразиты",
+            poster = R.drawable.parasites,
+            description = "Greed and class discrimination threaten the newly-formed symbiotic relationship between the wealthy Park family and the destitute Kim clan."
+        ),
+        Film(
+            title = "Отступники",
+            poster = R.drawable.otstupniki,
+            description = "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston."
+        ),
+        Film(
+            title = "Джанго освобожденный",
+            poster = R.drawable.django,
+            description = "With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation owner in Mississippi."
+        ),
+        Film(
+            title = "ВАЛЛ·И",
+            poster = R.drawable.valley,
+            description = "In the distant future, a small waste-collecting robot inadvertently embarks on a space journey that will ultimately decide the fate of mankind."
+        ),
+        Film(
+            title = "Бесславные ублюдки",
+            poster = R.drawable.lnglorious_bastards,
+            description = "In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner's vengeful plans for the same."
+        ),
+        Film(
+            title = "Вверх",
+            poster = R.drawable.up,
+            description = "78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway."
+        ),
+        Film(
+            title = "Зеленая книга",
+            poster = R.drawable.the_green_book,
+            description = "A working-class Italian-American bouncer becomes the driver for an African-American classical pianist on a tour of venues through the 1960s American South."
+        ),
+        Film(
+            title = "Кремниевая долина",
+            poster = R.drawable.kremnievaya_dolina,
+            description = "Follows the struggle of Richard Hendricks, a Silicon Valley engineer trying to build his own company called Pied Piper."
+        ),
+    )
 }
