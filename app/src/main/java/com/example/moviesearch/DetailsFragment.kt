8748b19b54096d9117bd0026727f3554481ed616
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2024-07-10 15:41:03 +0500

    add favorite and share

--- app/src/main/java/com/example/moviesearch/DetailsFragment.kt
+++ app/src/main/java/com/example/moviesearch/DetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.moviesearch
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import com.example.moviesearch.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
     private var _binding: FragmentDetailsBinding? = null
+    private lateinit var film: Film
     private val binding: FragmentDetailsBinding
         get() = _binding!!
 
@@ -30,10 +32,37 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setFilmsDetails()
+
+        binding.detailsFabFavorites.setOnClickListener {
+            if (!film.isInFavorites) {
+                binding.detailsFabFavorites.setImageResource(R.drawable.like)
+                film.isInFavorites = true
+            } else {
+                binding.detailsFabFavorites.setImageResource(R.drawable.baseline_favorite_border_24)
+                film.isInFavorites = false
+            }
+        }
+
+        binding.detailsFabShare.setOnClickListener {
+            //Создаем интент
+            val intent = Intent()
+            //Укзываем action с которым он запускается
+            intent.action = Intent.ACTION_SEND
+            //Кладем данные о нашем фильме
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Check out this film: ${film.title} \n\n ${film.description}"
+            )
+            //УКазываем MIME тип, чтобы система знала, какое приложения предложить
+            intent.type = "text/plain"
+            //Запускаем наше активити
+            startActivity(Intent.createChooser(intent, "Share To:"))
+        }
+
     }
 
     private fun setFilmsDetails() {
-        val film = arguments?.get("film") as Film
+        film = arguments?.get("film") as Film
 
         with(binding) {
             detailsToolbar.title = film.title
