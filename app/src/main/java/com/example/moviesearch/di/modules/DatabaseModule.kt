b4cbf2916a557d21fce8b85f66ebaacccc9a4630
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2024-12-18 19:30:44 +0500

    edit module

--- app/src/main/java/com/example/moviesearch/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/example/moviesearch/di/modules/DatabaseModule.kt
@@ -1,13 +1,19 @@
 package com.example.moviesearch.di.modules
 
+import android.content.Context
 import com.example.moviesearch.data.MainRepository
+import com.example.moviesearch.data.db.DatabaseHelper
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabaseHelper(context: Context) = DatabaseHelper(context)
+
     @Provides
     @Singleton
-    fun provideRepository() = MainRepository()
+    fun provideRepository(databaseHelper: DatabaseHelper) = MainRepository(databaseHelper)
 }
