748341b03f688b4fdcf598baab7f9fdeb2fe0159
Tatyana Abdulova <tatyanaabdulova@yandex.ru>
2025-05-17 00:57:49 +0500

    edit code

--- app/src/main/java/com/example/moviesearch/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/moviesearch/viewmodel/HomeFragmentViewModel.kt
@@ -26,4 +26,5 @@ class HomeFragmentViewModel : ViewModel() {
     fun getFilms() {
         interactor.getFilmsFromApi(1)
     }
+    fun getSearchResult(search: String) = interactor.getSearchResultFromApi(search)
 }
