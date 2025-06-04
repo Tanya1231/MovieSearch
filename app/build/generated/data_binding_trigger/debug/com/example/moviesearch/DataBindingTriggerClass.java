263acc76d18f2d380987825b54a109f50394581c
Tatyana Abdulova <tatyanaabdulova@yandex.ru>
2025-06-04 19:58:42 +0500

    edit

--- app/build/generated/data_binding_trigger/debug/com/example/moviesearch/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.moviesearch;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/src/main/java/com/example/moviesearch/API.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviesearch
-
-object API {
-    const val KEY = "b1fbddd0b6b7075659c6b6016d37acf0"
-}
